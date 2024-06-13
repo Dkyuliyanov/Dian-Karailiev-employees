6edb2527fe4eb770deef7ebf086e1d2508c06fb1
Dian Karailiev <dkyuliyanov@gmail.com>
2024-06-13 15:28:33 +0300

    added usage steps

--- src/main/java/org/sirma/diankarailievemployees/controllers/FileUploadController.java
+++ src/main/java/org/sirma/diankarailievemployees/controllers/FileUploadController.java
@@ -17,7 +17,6 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
 @Controller
